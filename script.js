669e606a5351cca1e1160d32f6d0cc53110bbc00
emmanuelogobuegwu <emmy1emmanuel@gmail.com>
2023-04-19 10:46:49 +0100

    updated

---
+++ script.js
@@ -0,0 +1,37 @@
+let product ={
+    name: " HP Victus",
+    description: " HP victus is a gaming laptop with a good system specificartion ",
+    price: 500000,
+    quantity: 2,
+}
+let employee ={
+    name: "emmanuel",
+    position: " manager",
+    salary: 2000000,
+}
+
+let job ={ 
+    name: "front-end developer",
+    description: " should be good with HTML,CSS & JAVA SCRIPT",
+    requirement: [  "< 2 years of expirence", ">26 of age", ]
+}
+/* name , author year published */
+let books =[ 
+    {name: "lost city",author: "ben fill",published: 2010},
+    {name: "lost boy",author: "bill soctfil",published: 2014},
+    {name: "lost man",author: "rings bell",published: 2007},
+]
+/*  id, name, department, result */
+let student ={
+    ID: 284755,
+    Name: "Emmanuel",
+    Department: " Mass Communication",
+    Result:{
+        Maths: 50, 
+        English: 90, 
+        BAM234: 79, 
+        MAS345: 69, 
+        COM325: 98
+    }
+}
+
